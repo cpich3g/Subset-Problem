dedff8cf6124a5c8f24da4368c87f0487180bf50
Justin Joy <justinjoy@icloud.com>
2015-03-05 23:00:03 +0000

    Initial Commit

---
+++ Subset Problem/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Subset Problem
+//
+//  Created by Justin Joy on 05/03/2015.
+//  Copyright (c) 2015 Coventry University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
